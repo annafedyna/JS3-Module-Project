fc38eb149bd11578c820da1f609e813e02c2934d
Anna Fedyna <annafedyna03@gmail.com>
2024-12-01 14:23:58 +0000

    level 100

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-//You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
+function addZero(num) {
+  return num < 10 ? '0' + num : num
+}
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach((episode) => {
+    newCard = document.createElement("div");
+    newCard.classList.add("card");
+    newCard.innerHTML = `<div class = "title-card">${episode.name} - S${addZero(
+      episode.season
+    )}E${addZero(episode.number)}</div>;
+    <img src="${episode.image.medium}" alt="${episode.name}" />
+    <p>${episode.summary}</p>`;
+    rootElem.append(newCard);
+  });
 }
 
 window.onload = setup;
